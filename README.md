f24b910a3acb2cd776e99e9eb408035491d8a5ce
Pete <git@kthxbye.us>
2020-02-02 19:42:14 +0100

    Initial commit of vpn tool

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # vpn_tool
+Tool to add openvpn files from NordVPN to Linux Network Manager. It also allows to benchmark different servers by cycling through all nordvpn servers and downloading a 10mb file. 
+
+## Usage
+vpn.py import
+
+Imports all .ovpn in the current directory where the script resides. It will ask for username and password and add it to network manager. It will not add duplicate entries. 
+
+vpn.py clear
+
+Deletes all nordvpn servers from network manager
+
+vpn.py benchmark
+
+Cycles through all nordvpn entries and downloads a 10mb with each. At the end it prints a list of the max average speed of each server. 
+
+## Motivation
+This tool allows to find the fastest server at the time of testing. 
---
+++ vpn.py
@@ -0,0 +1,184 @@
+#!/bin/python3
+
+import os
+import subprocess
+import argparse
+import getpass
+
+__version__ = '1.0.0'
+PATH = './'
+
+
+def runProcess(command):
+    # Invoke the command and pass its output streams to the completedProc object
+    # run()'s return value is an object with information about the completed process. 
+    completedProc = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Print the exit code.
+    #print("returned with exit code: %d" % (completedProc.returncode))
+    return completedProc
+
+def connectionExists(connection):
+    completedProcess = runProcess(["nmcli", "-f", "GENERAL.STATE", "con", "show", connection])
+    return completedProcess.returncode != 10
+
+def importConnection(connectionFile):
+    print("Importing Profile: '%s'" % (connectionFile))
+    runProcess(["nmcli", "connection", "import", "type", "openvpn", "file", connectionFile])
+
+def setUserAndPass(connection, user, password):
+    print("Setting user name and password for %s" % (connection))
+    runProcess(["nmcli", "connection", "modify", 
+                connection, 
+                "+vpn.data", "username=%s" % (user),
+                "+vpn.secrets", "password=%s" % (password)])
+    
+def getConnections(filter=None):
+    completedProcess = runProcess(["nmcli", "connection", "show"])
+    output = completedProcess.stdout
+    lines = output.splitlines()
+    nameLen = lines[0].find('UUID')
+    connections = []
+    for line in lines[1:]:
+        connection = line[:nameLen].strip()
+        if filter is None or connection.find(filter) != -1:
+            connections.append(connection)
+    return sorted(connections)
+
+def deleteConnection(connection):
+    print("Deleting: %s" % (connection))
+    runProcess(["nmcli", "connection", "delete", connection])
+    
+def deleteConnections(connections):
+    for connection in connections: 
+        deleteConnection(connection)
+        
+def importNewProfiles(searchPath):
+    vpnUsername = input("Enter your vpn username: ")
+    vpnPassword = getpass.getpass("Enter your vpn password: ")
+    connectionFiles = [f for f in os.listdir(searchPath) if os.path.isfile(os.path.join(searchPath, f)) and f.endswith('.ovpn')]
+
+    for connectionFile in connectionFiles: 
+        print(connectionFile)
+        connection = connectionFile[:-5]
+        print(connection)
+        if connectionExists(connection):
+            print("Connection '%s' exists - skipping" % (connection))
+            continue
+
+        importConnection(connectionFile)
+        setUserAndPass(connection, vpnUsername, vpnPassword)
+
+def downloadBenchmarkFile():
+    import requests
+    import time
+    import sys
+    start = time.time()
+    print("Starting Benchmark")
+    
+    timeout = 60
+    #remoteFile = "http://87.76.21.20/test.zip" # amsterdam
+    remoteFile = "http://fra36-speedtest-1.tele2.net/10MB.zip" # tele2 frankfurt
+    
+    try: 
+        # get file size: 
+        resGet = requests.get(remoteFile,stream=True)
+        fileSize = int(resGet.headers['Content-length']) # output 121291539
+        local_filename = "tmp.dat"
+        # NOTE the stream=True parameter below
+        timedOut = False
+        with requests.get(remoteFile, stream=True) as r:
+            r.raise_for_status()
+            for chunk in r.iter_content(chunk_size=81920): 
+                if chunk: # filter out keep-alive new chunks
+                    #f.write(chunk)
+                    print(len(chunk))
+                    # if time takes more than a minute we cancel
+                    current = time.time()
+                    if current - start > timeout:
+                        timedOut = True
+                        break
+                    sys.stdout.write("-")
+                    sys.stdout.flush()
+                    # f.flush()
+        print("")
+        if timedOut: 
+            # TODO: make this handling better
+            print("Timeout: Benchmark exceeded %fs" % (timeout))
+            return -1
+        end = time.time()
+        totalTime = end - start
+        kbS = fileSize / totalTime / 1024.
+        print("Finished Benchmark: %.3fs %.3fkb/s" % (totalTime, kbS))
+        return kbS
+    # catch keyboard and raise again to allow termination
+    except KeyboardInterrupt:
+        raise
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        return -1
+
+def connectConnection(connection):
+    print("Trying to connect: %s" % (connection))
+    completedProcess = runProcess(["nmcli", "con", "up", "id", connection])
+    # returns true if connection was successful
+    return completedProcess.returncode == 0
+
+def disconnectConnection(connection):
+    print("Trying to disconnect: %s" % (connection))
+    completedProcess = runProcess(["nmcli", "con", "down", "id", connection])
+    # returns true if connection was successful
+    return completedProcess.returncode == 0
+
+def benchmarkConnections(filter):
+    benchmarkResults = []
+    for connection in getConnections(filter):
+        kbS = benchmarkConnection(connection)
+        benchmarkResults.append([connection, kbS])
+    
+    for benchmarkResult in benchmarkResults:
+        connection = benchmarkResult[0]
+        kbS = benchmarkResult[1]
+        print("%s: %.3fkb/s" % (connection, kbS))
+    
+def benchmarkConnection(connection):
+    if not connectConnection(connection):
+        return -1
+    time = downloadBenchmarkFile()
+    disconnectConnection(connection)
+    return time
+
+def clear_cmd(args):
+    deleteConnections(getConnections("nordvpn.com"))
+
+def benchmark_cmd(args):
+    benchmarkConnections("nordvpn")
+    
+def import_cmd(args):
+    importNewProfiles(PATH)
+    
+if __name__ == '__main__':
+    print("NORDVPN Helper Tool " + __version__)
+    # main parser
+    parser = argparse.ArgumentParser(description='NordVPN Tooling')
+    subparsers = parser.add_subparsers()
+
+    # clear vpns
+    clear_parser = subparsers.add_parser('clear')
+    clear_parser.set_defaults(dispatch=clear_cmd)
+    
+    # add new vpns from the local path
+    import_parser = subparsers.add_parser('import')
+    import_parser.set_defaults(dispatch=import_cmd)
+    
+    # benchmark existing vpns
+    benchmark_parser = subparsers.add_parser('benchmark')
+    benchmark_parser.set_defaults(dispatch=benchmark_cmd)
+    
+    args = parser.parse_args()
+    if 'dispatch' in args:
+        cmd = args.dispatch
+        del args.dispatch
+        cmd(args)
+    else:
+        parser.print_usage()
